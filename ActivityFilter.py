f963e69458af5af46c593caac8198daae1579b97
JM Maro <maro@jmatthi.eu>
2018-12-10 17:04:50 +0100

    better _Initialize

--- ActivityFilter.py
+++ ActivityFilter.py
@@ -24,16 +24,17 @@ class ActivityFilter(Module):
         self.AllowedEvents = 0
         self.FilteredEvents = 0
 
+        StreamKey = self.__Framework__.StreamHistory[-1]
+        self._Xmax = self.__Framework__.StreamsGeometries[StreamKey][0]
+        self._Ymax = self.__Framework__.StreamsGeometries[StreamKey][1]
+        print("ActivityFilter: stream geometries are {0}, {1}".format(self._Xmax, self._Ymax))
+
         return True
 
     def _OnEvent(self, event):
-        # print("stream is {0}".format(self.__Framework__.StreamHistory[-1]))
-        # StreamKey = self.__Framework__.StreamHistory[-1]
-        # self._Xmax = self.__Framework__.StreamsGeometries[StreamKey][0]
-        # self._Ymax = self.__Framework__.StreamsGeometries[StreamKey][1]
-        # print("stream geometries: {0}, {1}".format(self._Xmax, self._Ymax))
 
-        if (event.location[0] > self._Radius) and (event.location[0] < (self.__Framework__.StreamsGeometries[self.__Framework__.StreamHistory[-1]][0] - self._Radius)) and (event.location[1] > self._Radius) and (event.location[1] < (self.__Framework__.StreamsGeometries[self.__Framework__.StreamHistory[-1]][1] - self._Radius)):
+
+        if (event.location[0] > self._Radius) and (event.location[0] < (self._Xmax - self._Radius)) and (event.location[1] > self._Radius) and (event.location[1] < (self._Ymax - self._Radius)):
             # extract neigborhood
             patch = np.copy(self._Memory.STContext[event.location[0] - self._Radius:event.location[0] + self._Radius + 1, event.location[1] - self._Radius:event.location[1] + self._Radius + 1, event.polarity])
             patch -= event.timestamp
